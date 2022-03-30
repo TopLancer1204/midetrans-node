3670a464de1768e31f1f0541f7625170cd885bed
smile20020322 <topfreelancer085.@gmail.com>
2022-03-30 17:57:12 +0300

    midtans

---
+++ 1.sh
@@ -0,0 +1,23 @@
+git filter-branch -f --env-filter '
+ 
+an=""$GIT_AUTHOR_NAME""
+am=""$GIT_AUTHOR_EMAIL""
+cn=""$GIT_COMMITTER_NAME""
+cm=""$GIT_COMMITTER_EMAIL""
+ 
+if [ ""$GIT_COMMITTER_EMAIL"" = ""restuwahyu13@gmail.com"" ] || [ ""$GIT_COMMITTER_EMAIL"" = ""restuwahyu13@gmail.com"" ]
+then
+cn=""toplancer1204""
+cm=""pro.developer1204@gmail.com""
+fi
+if [ ""$GIT_AUTHOR_EMAIL"" = ""restuwahyu13@gmail.com"" ] || [ ""$GIT_AUTHOR_EMAIL"" = ""restuwahyu13@gmail.com"" ]
+then
+an=""toplancer1204""
+am=""pro.developer1204@gmail.com""
+fi
+ 
+export GIT_AUTHOR_NAME=$an
+export GIT_AUTHOR_EMAIL=$am
+export GIT_COMMITTER_NAME=$cn
+export GIT_COMMITTER_EMAIL=$cm
+'
